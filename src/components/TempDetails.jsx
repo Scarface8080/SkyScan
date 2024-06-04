35068ae8ee72bd945bccb4efcccf94c1be2a7149
Scarface8080 <hamzashoukat8080@gmail.com>
2024-06-05 02:08:06 +0500

    woriking on Horizental details

--- src/components/TempDetails.jsx
+++ src/components/TempDetails.jsx
@@ -29,7 +29,37 @@ const TempDetails = () => {
             value: "11km/h",
         },
        
-    ]
+    ];
+
+  
+
+        const horizentalDetails = [
+            {
+                id:1,
+                icon: <GiSunrise />,
+                title: "Sunrise",
+                value: "05:33 AM",
+            },
+            {
+                id:2,
+                icon: <GiSunset />,
+                title: "Sunset",
+                value: "06:45 PM",
+            },
+            {
+                id:3,
+                icon: <MdKeyboardArrowUp />,
+                title: "High",
+                value: "37°",
+            },
+            {
+                id:4,
+                icon: <MdKeyboardArrowDown />,
+                title: "Low",
+                value: "7°",
+            },
+        ]
+    
 
 
 
@@ -40,25 +70,39 @@ const TempDetails = () => {
       </div>
 
       <div className="flex flex-row items-center justify-between py-3">
-        <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="weather icon" className="w-20"/>
+            <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="weather icon" className="w-20"/>
 
-        <p>34°</p>
+            <p>34°</p>
 
-        <div className="flex flex-col space-y-3 items-start">
+            <div className="flex flex-col space-y-3 items-start">
 
-                    {
-                        verticalDetails.map(({ id, icon, title, value }) => (
-                            <div key={id} className="flex font-light text-sm items-center justify-center">
-                            {icon}
-                            {`${title}:`} <span className="font-medium ml-1">{value} </span>
-                            </div>
-                        ))
-                    }
+                        {
+                            verticalDetails.map(({ id, icon, title, value }) => (
+                                <div key={id} className="flex font-light text-sm items-center justify-center">
+                                {icon}
+                                {`${title}:`} <span className="font-medium ml-1">{value} </span>
+                                </div>
+                            ))
+                        }
 
 
 
-           
-        </div>
+            
+            </div>
+      </div>
+
+      <div className="flex flex-row items-center justify-center space-x-10 text-sm py-3">
+            {
+                horizentalDetails.map(({ id, icon, title, value }) => (
+                    <div key={id} className="flex font-light text-sm items-center justify-center">
+                    {icon}
+                    {`${title}:`} <span className="font-medium ml-1">{value} </span>
+                    </div>
+                ))
+            }
+
+            <div className="flex flex-row items-center"></div>
+
       </div>
 
 
