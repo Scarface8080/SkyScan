7479c352c5fe4da5baac7376141d20d2b118cde2
Scarface8080 <hamzashoukat8080@gmail.com>
2024-06-04 21:12:59 +0500

    woriking on buttons search, Location & temp Btns

---
+++ src/components/Inputs.jsx
@@ -0,0 +1,34 @@
+import { BiSearch,BiCurrentLocation } from "react-icons/bi"
+
+
+const Inputs = () => {
+  return (
+    <div className="flex flex-row justify-center my-6">
+
+        <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
+
+
+            <input type="text" 
+            placeholder="search by city..."
+            className="text-gray-500 text-xl font-light p-2 w-full shadow-xl capitalize focus:outline-none placeholder:lowercase" />
+
+            <BiSearch size= {30}
+                      className="courser-pointer transition ease-out hover:scale-150"  />
+
+            <BiCurrentLocation size= {30}
+                      className="courser-pointer
+                       transition ease-out hover:scale-150"  />
+                       
+        </div>
+
+        <div className="flex flex-row w-1/4 items-center justify-center"></div>
+        <button className="text-l font-medium transition ease-out hover:scale-125">°C</button>
+
+
+       
+      
+    </div>
+  )
+}
+
+export default Inputs
