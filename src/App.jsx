41664cd2856c1aad976ee365619d566a1bb73bb4
Scarface8080 <hamzashoukat8080@gmail.com>
2024-06-04 20:40:40 +0500

    Testing Twailwind css and UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { FaReact } from "react-icons/fa";
+
 import React from 'react'
+import TopButtons from "./components/TopButtons";
 
 function App() {
   return (
-    <div>
-      
+    <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700">
+     <TopButtons />
     </div>
   )
 }
